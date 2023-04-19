11ed794a9a39320f7efaf30900fb7139c01bdc1e
Saidahror13 <01erkhinov@gmail.com>
2023-04-19 22:53:30 +0500

    project main update

--- login/models.py
+++ login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+# from django.utils.translation import gettext_lazy as _
+#
+#
+# class User(AbstractUser):
+#     email = models.EmailField(_("email address"), unique=True)
+#     phone_number = models.CharField(max_length=15, null=True, blank=True)
+#     birth_date = models.DateField(null=True, blank=True)
+#     profile_picture = models.ImageField(upload_to="profile_pictures", null=True, blank=True)
+#
+#     REQUIRED_FIELDS = []
+#
+#     def __str__(self):
+#         return self.username
--- login/views.py
+++ login/views.py
@@ -1,3 +1,95 @@
+# # from msilib.schema import ListView
+#
+# from django.contrib.auth import authenticate, login
+# from django.contrib.auth.views import LoginView
+# from django.contrib.messages import get_messages
+# from django.shortcuts import render, redirect
+# from django.views.generic import CreateView
+#
+# from login.forms import CustomAuthenticationForm, RegistrationForm
+# from login.forms import User
+#
+#
+# class AccountLoginView(LoginView):
+#     form_class = CustomAuthenticationForm
+#     template_name = "login/login.html"
+#
+#
+# # @login_required()
+# def custom_login(request):
+#     form = CustomAuthenticationForm(request.POST or None)
+#     if form.is_valid():
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
+#         if username is not None and password:
+#             user = authenticate(
+#                 request, username=username, password=password
+#             )
+#             login(request, user)
+#             return redirect("account:profile")
+#     return render(request, "login/login.html", {"form": form})
+#
+#
+# #
+# # class UserProfile(DetailView):
+# #     model = User
+# #     pk_url_kwarg = None
+# #     query_pk_and_slug = None
+# #
+# #     def get_queryset(self):
+# #         return self.request.user
+#
+# def profile(request):
+#     if request.user.is_authenticated:
+#         return render(request, "login/profile.html", {"user": request.user})
+#     return redirect("account:login")
+#
+#
+# class RegisterView(CreateView):
+#     model = User
+#     template_name = 'login/signup.html'
+#     form_class = RegistrationForm
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST or None)
+#         if form.is_valid():
+#             username = form.cleaned_data.get("username")
+#             first_name = form.cleaned_data.get("first_name")
+#             last_name = form.cleaned_data.get("last_name")
+#             email = form.cleaned_data.get("email")
+#             password = form.cleaned_data.get("password1")
+#             user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+#             login(request, user)
+#             return redirect("homepage")
+#         return render(request, "login/signup.html", {"form": form, 'messages': get_messages(request)})
+#
+# # class SearchView(ListView):
+# #     queryset = House.objects.all()
+# #     template_name = "search.html"
+# #     context_object_name = "results"
+# #
+# #
+# # def search(request):
+# #     search_query = request.POST.get("search")
+# #     expression = Q(title_icontain=search_query) | Q(title_icontain=search_query)
+# #     queryset = House.objects.filter(expression)
+# #     return render(request, "search.html", {"results": queryset})
+#
+# # def login(request):
+# #     form = CustomAuthenticationForm()
+# #     return render(request, "account/login.html", {"form": form})
+#
+#
+# # class AccountSignupView(LoginView):
+# #     form_class = CustomAuthenticationForm
+# #     template_name = "account/sign_up.html"
+#
+# # class AccountSignupView(generic.CreateView):
+# #     form_class = UserCreationForm
+# #     success_url = reverse_lazy('login')
+# #     template_name = 'account/sign_up.html'
+
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,4 +106,3 @@ class AccountLoginView(LoginView):
 class AccountRegisterView(LoginView):
     form_class = CustomAuthenticationForm
     template_name = 'login/signup.html'
-
