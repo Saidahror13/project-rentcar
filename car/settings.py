433057af25c368f77db52d9bd4ce5719b48daf46
Saidahror13 <01erkhinov@gmail.com>
2023-04-26 19:24:39 +0500

    static settings.py

--- car/settings.py
+++ car/settings.py
@@ -132,12 +132,13 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'static'
+else:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
